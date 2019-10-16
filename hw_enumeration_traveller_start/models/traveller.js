cfbc2e47918dbb62f3ddb20dd3632542cb2146b3
Charles Kennedy <charlesscottkennedy@googlemail.com>
2019-10-16 21:11:05 +0100

    MVP completed

---
+++ hw_enumeration_traveller_start/models/traveller.js
@@ -0,0 +1,30 @@
+const Traveller = function(journeys) {
+  this.journeys = journeys;
+};
+
+Traveller.prototype.getJourneyStartLocations = function() {
+  return this.journeys.map(journey => journey.startLocation);
+};
+
+Traveller.prototype.getJourneyEndLocations = function () {
+  return this.journeys.map(journey => journey.endLocation);
+};
+
+Traveller.prototype.getJourneysByTransport = function (value) {
+  return this.journeys.filter(journey => journey.transport === value);
+};
+
+Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
+  return this.journeys.filter(journey => journey.distance > minDistance);
+};
+
+Traveller.prototype.calculateTotalDistanceTravelled = function () {
+
+};
+
+Traveller.prototype.getUniqueModesOfTransport = function () {
+
+};
+
+
+module.exports = Traveller;
